4f2c34d12723da0491c575f9b73244da0b6d9ca0
jory999 <cmdeny21@gmail.com>
2020-11-19 22:30:45 +0800

    add a setupandinstall file

--- hello.go
+++ hello.go
@@ -5,6 +5,7 @@ import (
 	quoteV3 "rsc.io/quote/v3"
 )
 
+// Hello for this
 func Hello() string {
 
 	//return "Hello World" firm Git form vsCode
@@ -12,6 +13,7 @@ func Hello() string {
 	return quote.Hello()
 }
 
+// Proverb For loop that iterates over sharks list and prints each string item
 func Proverb() string {
 
 	return quoteV3.Concurrency()
